4f3e2200d92a16bdc3fd0b16d21b34590f51dbbf
zeflik <kontakt@jozefniemiec.pl>
2018-09-22 20:57:27 +0200

    Move provide Language Repository method from activity to app dagger module

--- app/src/main/java/pl/jozefniemiec/langninja/di/AppModule.java
+++ app/src/main/java/pl/jozefniemiec/langninja/di/AppModule.java
@@ -5,9 +5,17 @@ import android.content.Context;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
+import pl.jozefniemiec.langninja.repository.LanguageRepository;
+import pl.jozefniemiec.langninja.repository.RoomLanguageRepository;
 
 @Module
 public abstract class AppModule {
+
+    @Provides
+    static LanguageRepository provideLanguageRepository(Context context) {
+        return new RoomLanguageRepository(context);
+    }
 
     @Binds
     abstract Context provideContext(Application application);
--- app/src/main/java/pl/jozefniemiec/langninja/ui/main/MainActivityModule.java
+++ app/src/main/java/pl/jozefniemiec/langninja/ui/main/MainActivityModule.java
@@ -1,22 +1,14 @@
 package pl.jozefniemiec.langninja.ui.main;
 
-import android.content.Context;
 import android.support.v4.app.FragmentManager;
 
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
-import pl.jozefniemiec.langninja.repository.LanguageRepository;
-import pl.jozefniemiec.langninja.repository.RoomLanguageRepository;
 
 
 @Module
 public abstract class MainActivityModule {
-
-    @Provides
-    static LanguageRepository provideLanguageRepository(Context context) {
-        return new RoomLanguageRepository(context);
-    }
 
     @Provides
     static FragmentManager provideFragmentManager(MainActivity mainActivity) {
