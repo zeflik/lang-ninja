e9352ecf01a65501b5bd6318c1bcf6057eac6176
zeflik <kontakt@jozefniemiec.pl>
2018-10-22 21:29:23 +0200

    refactor package structure

---
+++ app/src/main/java/pl/jozefniemiec/langninja/ui/sentences/tts/OnInitListener.java
@@ -0,0 +1,22 @@
+package pl.jozefniemiec.langninja.ui.sentences.tts;
+
+import android.speech.tts.TextToSpeech;
+
+import javax.inject.Inject;
+
+import pl.jozefniemiec.langninja.ui.sentences.SentenceCardPresenter;
+
+public class OnInitListener implements TextToSpeech.OnInitListener {
+
+    public final SentenceCardPresenter presenter;
+
+    @Inject
+    OnInitListener(SentenceCardPresenter presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void onInit(int status) {
+        presenter.onReaderInit(status == TextToSpeech.SUCCESS);
+    }
+}
