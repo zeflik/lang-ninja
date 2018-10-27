c6645ded1d54fc3344790b13a639d20754b2ca6b
zeflik <kontakt@jozefniemiec.pl>
2018-10-27 10:34:10 +0200

    add remove punctuation form string utils

--- app/src/main/java/pl/jozefniemiec/langninja/utils/Utility.java
+++ app/src/main/java/pl/jozefniemiec/langninja/utils/Utility.java
@@ -4,9 +4,19 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 
 public class Utility {
+
+    private static final String PUNCTUATION_MARKS_REGEX = "[\"\',.!?¿:;]";
+
     public static int calculateNoOfColumns(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
         return (int) (dpWidth / 140);
+    }
+
+    public static String removePunctationMarks(String text) {
+        return text
+                .replaceAll(PUNCTUATION_MARKS_REGEX, "")
+                .trim()
+                .replaceAll("( )+", " ");
     }
 }
