97ce3427ea68c21e3dbbe76010e4f4036ffd21a9
zeflik <kontakt@jozefniemiec.pl>
2018-11-07 20:36:42 +0100

    add floating action button to send fragment

--- app/src/main/java/pl/jozefniemiec/langninja/ui/main/send/SendFragment.java
+++ app/src/main/java/pl/jozefniemiec/langninja/ui/main/send/SendFragment.java
@@ -2,16 +2,23 @@ package pl.jozefniemiec.langninja.ui.main.send;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import pl.jozefniemiec.langninja.R;
 
 public class SendFragment extends Fragment {
+
+    @BindView(R.id.floatingActionButtonAddSentence)
+    FloatingActionButton floatingActionButton;
 
     private Unbinder unbinder;
 
@@ -28,6 +35,12 @@ public class SendFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_send, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        floatingActionButton.setOnClickListener(v -> Toast.makeText(requireContext(), "TEst", Toast.LENGTH_SHORT).show());
     }
 
     @Override
